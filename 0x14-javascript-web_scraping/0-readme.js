7ab2bb6a571a465963e976aca85ab3214cb28838
jimoh1990 <jimohsikiru5@gmail.com>
2023-04-25 02:59:16 -0700

    Javascript is fun

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Reads and prints the contents of a file
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
